b7bf25a6f5d15eb6c4adc7a72e5b973210fcc0e8
Damian Pisanski <s27864@pjwstk.edu.pl>
2025-05-15 13:31:12 +0200

    Add contructor on Book,Reader class commit

---
+++ Book.py
@@ -0,0 +1,8 @@
+class Book:
+    def __init__(self, title, author,isbn,publisher,pages):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.publisher = publisher
+        self.pages = pages
+
---
+++ Reader.py
@@ -0,0 +1,7 @@
+class Reader:
+    def __init__(self,id,firstname,lastname,address,phonenumber):
+        self.id = id
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
+        self.phonenumber = phonenumber
